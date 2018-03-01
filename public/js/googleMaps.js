4d3d3e2386049bb93d871b3e505c83c0e77ce9c7
Abena Bonsu <aabonsu@ucsd.edu>
2018-03-01 08:57:03 -0800

    thirdpartyAPIlab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,18 @@
 function initMap() {
-	// add your code here
+
+	//Create center marker at UCSD
+	var ucsd_ltlng = {lat: 32.878849, lng: -117.235899};
+	//32.878849, -117.235899
+
+	//Create a center marker and specify DOM  elem for display
+  	var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: ucsd_ltlng,
+    map: map,
+    title: "UCSD"
+  });
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,13 @@ var data = {
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
+
+exports.addRSVP = function(req, res) {
+  var rsvpEmail = req.body.rsvpEmail;
+  //this will print in terminal when post is made
+  console.log(rsvpEmail);
+  //add to current data
+  data.rsvp.push(rsvpEmail);
+  //send task name back to the browser
+  res.send(rsvpEmail);
+}
